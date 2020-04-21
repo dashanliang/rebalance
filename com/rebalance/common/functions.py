54f3a343130c19a75b0e8db48d87f7a9f0bd81ca
dashanliang <dashan.liang@airwallex.com>
2020-04-21 23:20:53 +0800

    update for cc

--- com/rebalance/common/functions.py
+++ com/rebalance/common/functions.py
@@ -26,62 +26,116 @@ def changeToData(org, part=()):
     return datas
 
 
-print(getCutParts(6, 3))
+# print(getCutParts(6, 3))
 # simple filter the data
 
 
 # got the best funcion
 # check need level 2 / level 3 or not
+class x1:
+    value = 0.0
+
+class x2:
+    value = 0.0
+
+class x3:
+    value = 0.0
+
+class x4:
+    value = 0.0
+
+class x5:
+    value = 0.0
+
+class x6:
+    value = 0.0
+
+class x7:
+    value = 0.0
+
+class x8:
+    value = 0.0
+
+class x9:
+    value = 0.0
+
+class x10:
+    value = 0.0
+
 class x11:
     value = 0.0
 
-class x21:
-    value = 0.0
+# analysis all mh details
+ANZ_IN = [[x1], [x2], [x3]]
+ANZ_OUT = [[x4, x5], [x8, x10], []]
+ANZ_BALANCE = 0
 
-class x31:
-    value = 0.0
+CC_IN = [[x4], [], []]
+CC_OUT = [[], [], []]
+CC_BALANCE = 0
 
-class y11:
-    value = 0.0
+SCBHK_IN = [[x5], [x6], []]
+SCBHK_OUT = [[x7], [], [x3]]
+SCBHK_BALANCE = 0
 
-class z11:
-    value = 0.0
+SCBSG_IN = [[x7], [x8], []]
+SCBSG_OUT = [[x1], [x6], []]
+SCBSG_BALANCE = 1000
+SCBSG_NEED = 0
 
-class z21:
-    value = 0.0
+DBHK_IN = [[x9], [x10], []]
+DBHK_OUT = [[], [], []]
+DBHK_BALANCE = 0
 
-class k11:
-    value = 0.0
-
-class k21:
-    value = 0.0
-
-class p11:
-    value = 0.0
-
-class p21:
-    value = 0.0
-
-# x11 = x21 = x31 = y11 = z11 = z21 = k11 = k21 = p11 = p21 = 0.00
-allVariableComeIn = [[[x11], [x21], [x31]], [[y11]], [[z11], [z21]], [[k11], [k21]], [[p11], [p21]]]
-allVariableGoOut = [[[x11], [x21], [x31]], [[y11]], [[z11], [z21]], [[k11], [k21]], [[p11], [p21]]]
+DBSHK_IN = [[], [], []]
+DBSHK_OUT = [[x9], [x2], []]
+DBSHK_BALANCE = 500
+DBSHK_NEED = 0
 
 
-print(x11.value)
+# check level one is ok or not
+goal1 = len(ANZ_IN[0]) + len(CC_IN[0]) + len(SCBHK_IN[0]) + len(SCBSG_IN[0]) + len(DBHK_IN[0]) + len(DBSHK_IN[0])
 
-# objective function
+goalopt = np.ones(goal1 ,dtype = np.int)
+
+goalList = ANZ_IN[0] + CC_IN[0] + SCBHK_IN[0] + SCBSG_IN[0] + DBHK_IN[0] + DBSHK_IN[0]
+
+def getinequality(in_data = [x11], out_data = [x11]):
+    inequalityData = np.zeros(len(goalList), dtype= np.int)
+    for mh in in_data :
+        for index, mh_node in enumerate(goalList):
+            if mh.__name__ == mh_node.__name__ :
+                inequalityData[index] = 1
+                break
+
+    for mh in out_data :
+        for index, mh_node in enumerate(goalList):
+            if mh.__name__ == mh_node.__name__ :
+                inequalityData[index] = -1
+                break
+
+    return inequalityData
 
 
+# 对于anz cc 等 生成对应的 不等式
+anz = getinequality(ANZ_IN[0], ANZ_OUT[0])
 
-# x11+ y11 + z11 + k11 + p11
-# x11, y11, z11, k11, p11 >0
-# x11 =< scbsg
-# k11 + p11 =< scbhk
-# y11 + z11 =< x11
-# x11 -y11 - z11 >= anzbalance
-# y11 = cc balance
-# z11 - k11 -p11 >= scb balance
-# k11 - x11 >= scbsg balance
-# p11 >= dbhk balance
-# 根据条件判断 对应的系数
+cc = getinequality(CC_IN[0], CC_OUT[0])
 
+scbhk = getinequality(SCBHK_IN[0], SCBHK_OUT[0])
+
+scbsg = getinequality(SCBSG_IN[0], SCBSG_OUT[0])
+
+dbhk = getinequality(DBHK_IN[0], DBHK_OUT[0])
+
+dbshk = getinequality(DBSHK_IN[0], DBSHK_OUT[0])
+
+a = np.array([anz, cc, scbhk, scbsg, dbhk, dbshk])
+print(-a)
+b = np.array([20, 20, 20, 0, 50, 0])
+
+print(-b)
+print(goalopt)
+res = optimize.linprog(goalopt, A_ub=a, b_ub=b, bounds=((20, 1000), (20, 1500), (5, 1500), (50, 500), (50, 500)))
+
+print(res)
